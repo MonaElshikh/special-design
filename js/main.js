8b7c5263e2505e5eba1bb8917bce18ee4892e398
Mona Elshikh <mona.elshikh@hotmail.com>
2022-06-15 00:05:06 +0200

    adding and modifying skills and gallery sections

--- js/main.js
+++ js/main.js
@@ -4,27 +4,32 @@ const randomButtons = document.querySelectorAll(".random-btns span");
 const settingIcon = document.querySelector(".setting-icon");
 const settingsBox = document.querySelector(".settings");
 const landingSection = document.querySelector(".landing");
-const landingImgs = ["wallpaper-1.avif", "wallpaper-2.avif", "wallpaper-3.avif", "wallpaper-4.avif", "wallpaper-5.avif"];
+const landingImgs = ["wallpaper-1.jpg", "wallpaper-2.jpg", "wallpaper-3.jpg", "wallpaper-4.jpg", "wallpaper-5.jpg"];
 const rootElement = document.documentElement;
+const skillsSection = document.querySelector(".skills");
+const skillsProgress = document.querySelectorAll(".skill-progress span");
+let upLink = document.querySelector("body > a");
 let randonNo;
 let interval;
 //#endregion
 
 //#region Functions
+// funtion to remove active class from all elements inside the givin list.
 function reset(list) {
     list.forEach((item) => item.classList.remove("active"));
 }
 // funtion to show/hide setting box
 function toggleSettingsBox() {
-    settingIcon.addEventListener("click", () => {
+    settingIcon.addEventListener("click", (e) => {
         settingsBox.classList.toggle("show");
+        document.querySelector(".setting-icon .gear").classList.toggle("rotate");
     });
 }
 // funtion to load the current color stored in local storage
 function loadDefaultOptions() {
     // 1- Color Option
     // check local storage for default color
-    let colorMode = localStorage.getItem("color-mode");
+    let colorMode = localStorage.getItem("color-option");
     if (colorMode) {
         // set current color from local storage
         rootElement.style.setProperty("--main-color", colorMode);
@@ -41,7 +46,7 @@ function loadDefaultOptions() {
         rootElement.style.setProperty("--main-color", "#2196f3");
     }
     // 2- Random background option
-    let toggleImage = localStorage.getItem("toggle-imgs");
+    let toggleImage = localStorage.getItem("randomBg-option");
     if (toggleImage) {
         switch (toggleImage) {
             case "yes":
@@ -72,7 +77,7 @@ function toggleColorList() {
             // make the cliked color active
             color.classList.add("active");
             // save the chosen color to local storage.
-            localStorage.setItem("color-mode", e.target.dataset.color);
+            localStorage.setItem("color-option", e.target.dataset.color);
         });
     });
 }
@@ -84,15 +89,15 @@ function toggleBackground() {
             button.classList.add("active");
             if (button.innerHTML === "Yes") {
                 toggleLandingImages();
-                localStorage.setItem("toggle-imgs", "yes");
+                localStorage.setItem("randomBg-option", "yes");
             } else {
                 stopToggleLandingImages();
-                localStorage.setItem("toggle-imgs", "no");
+                localStorage.setItem("randomBg-option", "no");
             }
         });
     });
 }
-// funtion to set a random image for landing section.
+// funtion to set a random images for landing section.
 function toggleLandingImages() {
     interval = setInterval(() => {
         // get random number within array lenght
@@ -101,9 +106,73 @@ function toggleLandingImages() {
         landingSection.style.backgroundImage = 'url("/imgs/' + landingImgs[randonNo] + '")';
     }, 5000);
 }
+// function to stop random background images for landing section
 function stopToggleLandingImages() {
     clearInterval(interval);
-    landingSection.style.backgroundImage = 'url("/imgs/' + landingImgs[0] + '")';
+}
+// funtion to animate skills on window scrolling
+function animateSkills() {
+    window.onscroll = function () {
+        // get skills section offset top
+        let skillsOffsetTop = skillsSection.offsetTop;
+        // get skills section height
+        let skillsHeight = skillsSection.offsetHeight;
+        // get window outer height
+        let windowHeight = this.outerHeight;
+        // get page scrollY offset
+        let windowScrollY = window.scrollY;
+        // check if scroll > 700 show up link
+        windowScrollY > 700 ? upLink.style.display = "block" : upLink.style.display = "none";
+        if (windowScrollY > (skillsOffsetTop + skillsHeight - windowHeight)) {
+            skillsProgress.forEach(skill => {
+                skill.style.width = skill.dataset.progress;
+            });
+        }
+    }
+}
+// function to open popup for each gallery img on click
+function openGalleryImgPopup() {
+    let imges = document.querySelectorAll(".imges-box img");
+    // console.log(imges);
+    imges.forEach(img => {
+        img.addEventListener("click", (e) => {
+            // create the modal popup
+            let modal = document.createElement("div");
+            modal.className = "modal";
+            // create modal content
+            let modalContent = document.createElement("div");
+            modalContent.className = "modal-content";
+            // create modal header
+            let modalHeader = document.createElement("h2");
+            modalHeader.innerHTML = e.target.alt || "unTitled"
+            modalHeader.className = "modal-header";
+            // create close button
+            let closeButton = document.createElement("span");
+            let closeText = document.createTextNode("x");
+            closeButton.appendChild(closeText);
+            closeButton.className = "close";
+            // create modal image
+            let modalImage = document.createElement("img");
+            modalImage.src = e.target.src;
+            // append elements to parents
+            modalContent.appendChild(modalHeader);
+            modalContent.appendChild(closeButton);
+            modalContent.appendChild(modalImage);
+            modal.appendChild(modalContent);
+            document.body.appendChild(modal);
+        });
+    });
+}
+// function to close popups
+function closePopups() {
+    document.addEventListener("click", (e) => {
+        if (e.target.className === "close") {
+            e.target.parentNode.parentNode.remove();
+        }
+        else if (e.target.className === "modal") {
+            e.target.remove();
+        }
+    });
 }
 //#endregion
 
@@ -112,4 +181,7 @@ loadDefaultOptions();
 toggleSettingsBox();
 toggleColorList();
 toggleBackground();
+animateSkills();
+openGalleryImgPopup();
+closePopups();
 //#endregion
